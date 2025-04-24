ce6bb978c1d88446414a71e7231f39861aeeb388
sylvain-d-guimond <sylvain.d.guimond@gmail.com>
2025-04-24 17:06:51 +0200

    First public alpha

--- Assets/Scripts/Helper/PlatformBasedOffset.cs
+++ Assets/Scripts/Helper/PlatformBasedOffset.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class PlatformBasedOffset : MonoBehaviour
 {
     [SerializeField] private Vector3 AndroidOffset;
-    [SerializeField] private Vector2 UWPOffset;
+    [SerializeField] private Vector3 UWPOffset;
+
+    [SerializeField] private bool OverrideAndroid;
+    [SerializeField] private bool OverrideUWP;
 
     private void Start()
     {
 #if UNITY_ANDROID
-        transform.position += AndroidOffset;
+#if UNITY_EDITOR
+        if (!OverrideUWP)
+#endif
+            transform.position += AndroidOffset;
 #endif
 
 #if UNITY_WSA
-        transform.position += UWPOffset;
+#if UNITY_EDITOR
+        if (!OverrideAndroid)
+#endif
+            transform.position += UWPOffset;
+#endif
+
+#if UNITY_EDITOR
+        if (OverrideAndroid)
+            transform.position += AndroidOffset;
+        else if (OverrideUWP)
+            transform.position += UWPOffset;
 #endif
     }
 }
